8268158c2f6b8100fc1389928e18a5a06474d22d
valentin-ortiz <red.line.v.o.s@outlook.com>
2026-03-26 21:57:44 -0600

    Revert "Actualización de agentes y configuración"
    This reverts commit 9f0dd48234ecc3cad2156be85ba36aee86d79b90.

--- app/settings/settings.py
+++ app/settings/settings.py
@@ -20,7 +20,7 @@ class Settings(BaseSettings):
     LLM_THINKING: bool = os.getenv("LLM_THINKING", "false").lower() == "true"
     LLM_THINKING_BUDGET: int = int(os.getenv("LLM_THINKING_BUDGET", "1024"))
     SEARXNG_HOST: str = os.getenv("SEARXNG_HOST", "http://127.0.0.1:8888")
-    HITL_ASK_FOR_READ: bool = os.getenv("HITL_ASK_FOR_READ", "false").lower() == "true"
+    HITL_ASK_FOR_READ: bool = os.getenv("HITL_ASK_FOR_READ", "true").lower() == "true"
 
 
 settings = Settings()
