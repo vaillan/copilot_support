deb9dfc569f176394185e9f6d226a78bd5efdbbd
valentin-ortiz <red.line.v.o.s@outlook.com>
2026-03-26 21:57:29 -0600

    Revert "Actualización de agentes y archivos de configuración"
    This reverts commit f0a92e0b151e5168b134715ca80296517982cd22.

--- app/main.py
+++ app/main.py
@@ -21,6 +21,7 @@ def crear_grafo():
     # (Cerebros)
     workflow.add_node("agente_planificador", agente_planificador)
     workflow.add_node("agente_codificador", agente_codificador)
+    workflow.add_node("agente_codificador_silent", agente_codificador)
     workflow.add_node("agente_revisor", agente_revisor)
     workflow.add_node("agente_documentador", agente_documentador)
 
